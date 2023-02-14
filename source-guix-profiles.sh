7227699eea3cf78a26bf702ea7ac532e99e9dcb5
Mitchell Schmeisser <mitchellschmeisser@librem.one>
2023-02-13 22:49:29 -0500

    script actually sources profiles now

--- source-guix-profiles.sh
+++ source-guix-profiles.sh
@@ -1,10 +1,17 @@
 GUIX_EXTRA_PROFILES=$HOME/.extra-guix-profiles
 
 for i in $GUIX_EXTRA_PROFILES/*; do
-    profile=$i/$(basename "$i")
-    if [ -f "$profile"/etc/profile ]; then
-	GUIX_PROFILE="$profile"
-	. "$GUIX_PROFILE"/etc/profile
-    fi
-    unset profile
+	profile=$(basename "$i")
+	#skip old generations
+	if  [[ $profile =~ "-link" ]]; then
+		continue
+	fi
+
+	if [ -f "$i"/etc/profile ]; then
+		GUIX_PROFILE="$i"
+		. "$GUIX_PROFILE"/etc/profile
+	else
+		echo "no /etc/profile found in $i"
+	fi
+	unset profile
 done
