e5db85dc4598e6ba3c19d566e9f5a7a7381c3d9e
Mitchell Schmeisser <mitchellschmeisser@librem.one>
2023-06-20 10:24:45 -0400

    emacs/lisp/web.el: deleted

--- emacs/lisp/web.el
+++
@@ -1,7 +0,0 @@
-(defun prot-eww--rename-buffer ()
-  "Rename EWW buffer using page title or URL.
-    To be used by `eww-after-render-hook'."
-  (let ((name (if (eq "" (plist-get eww-data :title))
-		  (plist-get eww-data :url)
-		(plist-get eww-data :title))))
-    (rename-buffer (format "*%s # eww*" name) t)))
