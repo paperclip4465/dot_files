0690f0376b7a906bd877aed726ba6fc96831cadd
Mitchell Schmeisser <mitchellschmeisser@librem.one>
2024-09-10 16:03:18 -0400

    updated window management stuff

--- emacs/mfs-modules/mfs-wm.el
+++ emacs/mfs-modules/mfs-wm.el
@@ -1,43 +1,42 @@
 (setq display-buffer-alist '())
 
 ;; Get info/man windows to open in a nice way...
-(add-to-list 'display-buffer-alist
-	     `(,(rx (or "*info*"
-			"*Help*"
-			"WoMan"))
-	       (display-buffer-in-side-window)
-	       (side . right)
-	       (slot . 0)
-	       (window-width . 100)
-	       (window-parameters
-		(no-delete-other-windows . t))))
+(setq display-buffer-alist
+      `((,(rx (or "*info*"
+		  "*Help*"
+		  "WoMan"))
+	 (display-buffer-in-side-window)
+	 (side . right)
+	 (slot . 0)
+	 (window-width . 100)
+	 (dedicated . t)
+	 (window-parameters
+	  (mode-line-format . none)
+	  (no-delete-other-windows . t)))
 
-;; get compilation buffers to pop up on bottom
-(add-to-list 'display-buffer-alist
-	     `(,(rx (| "compilation"
-		       "xref"))
-	       (display-buffer-reuse-mode-window
-		display-buffer-in-side-window)
-	       (reusable-frames . t)
-	       (side . bottom)
-	       (slot . 0)
-	       (window-height . 20)
-	       (window-parameters
-		(no-delete-other-windows . t))))
+	(,(rx (| "compilation"
+		 "xref"))
+	 (display-buffer-reuse-mode-window
+	  display-buffer-in-side-window)
+	 (dedicated . t)
+	 (reusable-frames . t)
+	 (side . bottom)
+	 (slot . 0)
+	 (window-height . 20)
+	 (window-parameters
+	  (no-delete-other-windows . t)))
 
-;; Reuse windows for these things
-(add-to-list 'display-buffer-alist
-	     `(,(rx (| "*xref*"
-		       "*grep*"
-		       "*Occur*"))
-	       display-buffer-reuse-window
-	       (inhibit-same-window . nil)))
+	(,(rx (| "*xref*"
+		 "*grep*"
+		 "*Occur*"))
+	 display-buffer-reuse-window
+	 (inhibit-same-window . nil))
 
-;; Make magit-revision windows split horizontally and open below
-(add-to-list 'display-buffer-alist
-	     `(,(rx (| "magit-revision"))
-	       display-buffer-below-selected
-	       (window-height . 100)))
+	(,(rx (| "magit-revision"))
+	 (display-buffer-reuse-mode-window
+	  display-buffer-below-selected)
+	 (window-height . 100)
+	 (dedicated . t))))
 
 (require 'windmove)
 
